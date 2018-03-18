c3bc3a841178e92451bab0c777c446b4be722d79
JoeAmedeo <joseph_amedeo@yahoo.com>
2018-03-18 16:15:25 +0000

    GET request tested and functional

--- API/server.js
+++ API/server.js
@@ -1,22 +1,28 @@
 var express = require('express');
-var body-parser = require('body-parser');
+var bodyParser = require('body-parser');
 var mysql = require('mysql')
 
 var connection = mysql.createConnection({
 	host: "localhost",
 	user: "root",
-	password: "something"
+	password: "",
+	database: "SensorData"
 });
 
 var app = express();
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
 	connection.connect(function(err) {
 		if (err) throw err;
 		console.log("connected to db");
-		connection.query("SELECT * FROM Data ORDER BY Time DESC LIMIT 1", function(err, result) {
+		connection.query("(SELECT * FROM Data WHERE GPIO_number = 17 ORDER BY Time DESC LIMIT 1) UNION (SELECT * FROM Data WHERE GPIO_number = 18 ORDER BY Time DESC LIMIT 1)", function(err, result) {
 			if (err) throw err;
 			console.log(result);
+			res.send(result);
 		});
 	});
 });
+
+app.listen(8080);
