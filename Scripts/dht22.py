9371fb6037c57581b43e525f914e1a80bfc807cb
Joe Amedeo <joseph_amedeo@yahoo.com>
2018-07-11 16:43:48 -0400

    checked data saving/deleting functionality

--- Scripts/dht22.py
+++ Scripts/dht22.py
@@ -3,35 +3,49 @@ import RPi.GPIO as GPIO
 from time import sleep
 import datetime
 import Adafruit_DHT
-import MySQLdb
+import requests
 
-# establish databse connection and cursor for querying
-db = MySQLdb.connect(host="localhost", user="root", passwd="something", db="SensorData")
+#url for your api
+url = "insert url for api here"
+
+#the numbers you want to attribute each sensor in the database
+#can be changed if, for example, you hook up two pi's to a database and want to differentiate all 4 sensors
+sensor1 = 1
+sensor2 = 2
 
 #utilize adafruit library to retrieve data from humidity/temp sensors
 def getSensorData(GPIO_number):
     Humidity, Tempurature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, GPIO_number)
     print(str(Humidity), str(Tempurature))
     return (Humidity, Tempurature)
-def createTableEntry(GPIO_number, time, humidity, tempurature):
-    try:
-        cursor = db.cursor()
-        cursor.execute("INSERT INTO Data VALUES ('" + str(GPIO_number) + "', '" + str(time) + "', '" + str(humidity) + "', '" + str(tempurature) + "')")
-        db.commit()
-    except:
-        db.rollback()
-    return 0
-    
+
+#put data gathered from this pi into the database
+def createTableEntry(sensorNumber, time, humidity, tempurature):
+    requests.put(url, json={'sensorNumber': sensorNumber, 'time': time, 'humidity': humidity, 'tempurature': tempurature})
+
+#check to see if our database is getting too large (struggles of free mongodb hosting)
+#remove oldest entries if the we are getting close to our data limit
+def checkTableSize():
+    collectionCount = requests.get(url)
+    if collectionCount > 60000:
+        requests.delete(url, json={'sensorNumber': sensor1})
+        requests.delete(url, json={'sensorNumber': sensor2})
+
 # main function will run indefinately until an exception occurs. It will gather information from your sensors, push data to the database, and put the thread to sleep for 5 minutes.
 def main():
     while True:
         try:
-            Humidity17, Tempurature17 = getSensorData(17)
-            Humidity18, Tempurature18 = getSensorData(18)
-            
+            #NOTE: do not use sensor variables for input, alter inputs to these two calls according to the gpio number used in your pi
+            Humidity1, Tempurature1 = getSensorData(17)
+            Humidity2, Tempurature2 = getSensorData(18)
+
             currentDateTime = datetime.datetime.now()
-            createTableEntry(17, currentDateTime, Humidity17, Tempurature17)
-            createTableEntry(18, currentDateTime, Humidity18, Tempurature18)
+
+            createTableEntry(sensor1, currentDateTime, Humidity1, Tempurature1)
+            createTableEntry(sensor2, currentDateTime, Humidity2, Tempurature2)
+
+            checkTableSize()
+
             sleep(900)
         except Exception as inst:
             print type(inst)
@@ -40,7 +54,6 @@ def main():
             x, y = inst.args
             print "x = ", x
             print "y = ", y
-            db.close()
             break
             
 if __name__ == '__main__':
